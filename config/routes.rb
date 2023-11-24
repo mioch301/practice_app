6e71ce747b1b1f1d1ad01f04621a5548edd56475
mioch301 <deviluke564@gmail.com>
2023-11-24 23:21:32 +0000

    7commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
   get 'lists/:id' => 'lists#show'
+  get 'lists/:id' => 'lists#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
