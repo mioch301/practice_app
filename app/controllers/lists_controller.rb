67860f08a19530480010b8e8e15e12baf01f037e
mioch301 <deviluke564@gmail.com>
2023-11-26 04:24:02 +0000

    8commit

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,6 +10,14 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])  
   end
 
+  def create
+    list = List.new(list_params)
+    list.save
+    # redirect_to '/top' を削除して、以下コードに変更
+    # 詳細画面へリダイレクト
+    redirect_to list_path(list.id)  
+  end
+
   def edit
   end
 end
